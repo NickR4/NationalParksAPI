daac0cd59302058eafdc50b9a318d1986ce36c4f
NickR4 <72036401+NickR4@users.noreply.github.com>
2021-03-03 23:10:28 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,31 +1,37 @@
 'use strict';
 
-var state = document.getElementById("stateID").value;
-var count = document.getElementById("resultsLimit").value;
 const apiKey = 'g7OL9gTmepIfiTvle3meOMBXkc5z38ydtLTUNu5c';
-const searchURL = 'https://developer.nps.gov/api/v1/parks?stateCode=';
-var fetchURL = '';
+const searchURL = 'https://developer.nps.gov/api/v1/parks';
+var searchString = "";
 
-function formatQueryParams(){
-  
-  fetchURL = searchURL+state+'&limit='+count+'&api_key='+apiKey;
+function createSearchString(state,count){
+ 
+  console.log('create searchString function running');
+  searchString = `${searchURL}?stateCode=${encodeURIComponent(state)}&limit=${encodeURIComponent(count)}&api_key=${encodeURIComponent(apiKey)}`;
+
 }
 
 function findPark(){
-    fetch(fetchURL)
+  var state = document.getElementById("stateID").value;
+  var count = document.getElementById("resultsLimit").value;
+
+  console.log("find park function triggered");
+   createSearchString(state,count);
+    fetch(searchString)
     .then(response => response.json())
-    .then(responseJson => displayParks(responseJson)); 
-    
+    .then(responseJson => displayParks(responseJson))
+    .catch(error => alert('Something went wrong. Try again later.'));
 }
 
 
+
+
 function displayParks(responseJson){
-  
    $('#results-list').empty();
   for (let i = 0; i < responseJson.data.length; i++){
     $('#results-list').append(
       `<li><h3>${responseJson.data[i].fullName}</h3>
-     
+      <h5>${responseJson.data[i].addresses[0].city}, ${responseJson.data[i].addresses[0].stateCode}</h5>
       <p>${responseJson.data[i].description}</p>
       <h4><a href="${responseJson.data[i].url}">${responseJson.data[i].url}</a></h4>
       </li>`
@@ -35,7 +41,8 @@ function displayParks(responseJson){
 function watchForm() {
   $('form').submit(event => {
     event.preventDefault();
-    formatQueryParams();
+    
+    console.log('Button pushed, watch form function triggered');
     findPark();
     
   });}
@@ -43,3 +50,5 @@ function watchForm() {
 $(function() {
   watchForm();
 });
+
+//how to access and display and manipulate the addresses nest array
